bb22d2617cdeff955cd7fc19ab2964245f08ef95
Fernando Davi <92491040+fnddavi@users.noreply.github.com>
2025-07-20 17:08:08 -0300

    Exercicios Phishing

---
+++ aula02/login_cps_page_4phishing/backend/src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import { setupSwagger } from "./swagger";
+import loginRoutes from "./routes/loginRoutes";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+setupSwagger(app);
+
+app.use("/", loginRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Documentação disponível em http://localhost:${PORT}/docs`);
+});
